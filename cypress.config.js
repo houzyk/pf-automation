fc4f0c3b6e1c357a2f162ce1ea973415e96887a5
Houzair Koussa <houzairmk@icloud.com>
2023-03-22 20:13:38 +0400

    [dev] ADD env variables to cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
     },
-    baseUrl: "https://houzyk.github.io/houzair/"
+    baseUrl: "https://houzyk.github.io/houzair/",
+    env: {
+      CWV_API_KEY: process.env.CWV_API_KEY
+    }
   },
 });
